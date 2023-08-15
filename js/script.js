059b9fd488ef0ee47bae0917e4f27ad23896563a
kordy21 <m.abdelhamed941@gmail.com>
2023-08-15 08:50:44 +0300

    Add_Latest_Blog

--- js/script.js
+++ js/script.js
@@ -9,10 +9,38 @@ $(document).ready(function(){
   h=document.querySelector("#c-scroll")
   window.addEventListener('scroll',function(){
     if (this.document.body.scrollTop > 40 || this.document.documentElement.scrollTop > 40 ){
-        console.log(h)
         h.classList.add("hidden")
+        console.log(this.document.documentElement.scrollTop)
     }
     else{
         h.classList.remove("hidden")
     }
   })
+//   
+let total_project =document.querySelector("#total_project")
+let Won =document.querySelector("#Won")
+let Happy =document.querySelector("#Happy")
+let exp =document.querySelector("#exp")
+function counter (tag , endConter , time ,incre ){
+    let i =0
+        let set =setInterval(()=>{
+            if (i==endConter) { 
+                clearInterval(set)
+            }
+            tag.innerText = i
+            i+=incre
+        } ,time)
+
+}
+    var test =0
+    window.addEventListener("scroll" ,function(){
+        if (this.document.body.scrollTop > 900 || this.document.documentElement.scrollTop > 900 ){
+            if (test==0){
+                counter(total_project,8000 ,10 ,50)
+                counter(Won,810 ,10 ,10)
+                counter(Happy,2000 ,15 ,20)
+                counter(exp,20 ,80 ,1)
+                test++ 
+            }
+            }
+})
